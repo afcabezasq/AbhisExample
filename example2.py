6832236f75b57ac8aadb812f3795ec15ba50b2e5
abhijayp <abhijay.potluri@gmail.com>
2026-05-09 19:41:22 -0400

    Latest Changes

--- example2.py
+++ example2.py
@@ -1,1 +1,4 @@
 print("Another example")
+print("Hi")
+print("My name is abhijay")
+print("I like using Rasberry Pi, ESP32, and Arduino")
--- myFile.py
+++ myFile.py
@@ -1,1 +1,7 @@
 print("This is some example")
+print("Hi this is abhijay")
+y = input("What is ur name?")
+if y == "john":
+    print("I like john")
+else: 
+    print("I wish you were a john")
